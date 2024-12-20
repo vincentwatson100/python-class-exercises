101f6617455dee818bb4f9631b543b8e50c5f3f5
Vincent <Vincewat5000@gmail.com>
2024-12-20 15:51:10 -0600

    step 2 completed

---
+++ wordle_project.py
@@ -0,0 +1,6 @@
+import random
+
+word_list = ['fizzy', 'crazy', 'apple', 'berry', 'black', 'white', 'cozey', 'alone', 'along', 'alert', 'alike', 'allow', 'alter', 'exist', 'extragoing', 'hence', 'globe', 'final', 'first', 'fixed', 'loose', 'logic', 'quiet', 'speak', 'spare', 'grape', 'peach']
+
+chosen_word = random.choice(word_list)
+guess=input('please enter a five letter word: ')
